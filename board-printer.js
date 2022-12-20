e2db4eb5ce172ef432811ac69d797075cb671330
Olena Reukova <kronverk01@gmail.com>
2022-12-20 18:18:51 +0000

    board -printer
    changed values in array of array by using indexes

--- board-printer.js
+++ board-printer.js
@@ -15,13 +15,36 @@
         =================
     Test your function by calling it with an example tic-tac-toe board.
 */
-export function printBoard(board) {
-}
+
+let board = [
+  ["X", "0", "X"],
+  ["X", "0", "X"],
+  ["0", "X", "0"],
+];
+console.log(board);
+
+// we change 'X' into '_'  in the first row and second column place (index [0][1] in array)
+board[0][1] = "_";
+// we change 'X' into '_'  in the first row and third column place (index [0][2] in array)
+board[0][2] = "_";
+// we change 'X' into '_'  in the second row and first column place (index [1][0] in array)
+board[1][0] = "_";
+// we change '0' into 'X'  in the second row and second column place (index [1][1] in array)
+board[1][1] = "X";
+// we change 'X' into '_'  in the second row and third column place (index [1][2] in array)
+board[1][2] = "_";
+// we change 'X' into '0'  in the third row and second column place (index [2][1] in array)
+board[2][1] = "0";
+// we change '0' into 'X'  in the third row and third column place (index [2][2] in array)
+board[2][2] = "X";
+
+console.log(board);
+
+export function printBoard(board) {}
 
 /*
     Given a tic-tac-toe board (an array of arrays),
         - return true if there are no moves left to make (there are no more '_' values)
         - return false if there are still moves that can be made
 */
-export function checkIfNoMovesLeft(board) {
-}
+export function checkIfNoMovesLeft(board) {}
